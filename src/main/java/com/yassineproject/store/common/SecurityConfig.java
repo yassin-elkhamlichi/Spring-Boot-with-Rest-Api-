6c7e7ca2a9c7fe1a9b424c266a86af21ea83ef65
yassin-elkhamlichi <yassinelkhamlichi98@gmail.com>
2025-12-03 17:45:18 +0100

    add cros config

--- src/main/java/com/yassineproject/store/common/SecurityConfig.java
+++ src/main/java/com/yassineproject/store/common/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
                                     .setStatus(HttpStatus.FORBIDDEN.value())));
                         });
                 return http.build();
-                --
+
         }
 
 }
